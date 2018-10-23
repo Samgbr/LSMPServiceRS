5ff0a70466431fa7810eabe4e21bda0b72c285fb
Nasr1994 <nhussaini@luc.edu>
2018-10-22 19:05:11 -0500

    ProductRepresentation and ProductRequest are added

---
+++ src/com/lsmp/mp/product/service/representation/ProductRepresentation.java
@@ -0,0 +1,58 @@
+package com.lsmp.mp.product.service.representation;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "ProductRequest")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+
+public class ProductRepresentation {
+	private String productID;
+	private String productName;
+	private String description;
+	private double purchasePrice;
+	private double sellingPrice;
+	private double discount;
+		
+	public String getProductID() {
+		return productID;
+	}
+	public void setProductID(String productID) {
+		this.productID = productID;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPurchasePrice() {
+		return purchasePrice;
+	}
+	public void setPurchasePrice(double purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+	public double getSellingPrice() {
+		return sellingPrice;
+	}
+	public void setSellingPrice(double sellingPrice) {
+		this.sellingPrice = sellingPrice;
+	}
+	public double getDiscount() {
+		return discount;
+	}
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+
+}
